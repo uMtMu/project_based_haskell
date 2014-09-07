410ce88d11f90373b6638053cc02de082161bc03
uMtMu <umutcakir@itu.edu.tr>
2014-09-08 00:09:55 +0300

    discount eklendi

--- ch2.hs
+++ ch2.hs
@@ -8,9 +8,9 @@ data Person = Person String String Gender
 
 data Gender = Male | Female | Unknown deriving Show
 
-data TMDirection = Past | Future | Both
+data TMDirection = Past | Future | Both deriving Show
 
-data TimeMachine = TimeMachine String String Integer String TMDirection Float
+data TimeMachine = TimeMachine String Integer String TMDirection Float deriving Show
 
 clientName :: Client -> Maybe String
 clientName client = case client of
@@ -33,7 +33,13 @@ genderCounter clients = printGender $ genderIdentify (0, 0) clients
                                             Female -> genderIdentify (m, f + 1) xs
                                     _                            -> genderIdentify (m, f) xs
                                 printGender (m, f) = "we have " ++ show m ++ " male and " ++ show f ++ " female client"
+
+discountTimeMachine percentage tms = map (discount percentage) tms
+                                        where discount percentage (TimeMachine manufacturer model name direction price) = TimeMachine manufacturer model name direction (price * percentage)
+    
 main = do let clients = [GovOrg "NSA",
                         Company "CIA" 12 (Person "John" "Nash" Male) "Head",
-                        Individual (Person "Eric" "Bana" Male) True] 
-             in print $ genderCounter clients
+                        Individual (Person "Eric" "Bana" Male) True]
+              timeMachines = [TimeMachine "EasyGo" 1999 "Go1" Past 100.50,
+                              TimeMachine "EasyGo" 2050 "Go5" Future 2500.75]
+             in print $ discountTimeMachine 0.80 timeMachines
